e425f0e9e6d36e321dba2cfe1cc1f635d0eee5fe
Harsh Kumar Raghav <raghavharsh068@gmail.com>
2023-07-17 21:59:01 +0530

    profolio update

--- Experience/Camera.js
+++ Experience/Camera.js
@@ -54,22 +54,16 @@ export default class Camera {
     this.perspectiveCamera.aspect = this.sizes.aspect;
     this.perspectiveCamera.updateProjectionMatrix();
 
-    this.orthographicCamera.left =
-      (-this.sizes.aspect * this.sizes.frustrum) / 2,
-      this.orthographicCamera.right =
-        (this.sizes.aspect * this.sizes.frustrum) / 2,
-      this.orthographicCamera.top = this.sizes.frustrum / 2,
-      this.orthographicCamera.bottom = -this.sizes.frustrum / 2,
+    (this.orthographicCamera.left =
+      (-this.sizes.aspect * this.sizes.frustrum) / 2),
+      (this.orthographicCamera.right =
+        (this.sizes.aspect * this.sizes.frustrum) / 2),
+      (this.orthographicCamera.top = this.sizes.frustrum / 2),
+      (this.orthographicCamera.bottom = -this.sizes.frustrum / 2),
       this.orthographicCamera.updateProjectionMatrix();
   }
 
   update() {
-    // console.log(this.perspectiveCamera.position);
     this.controls.update();
-
-    // this.helper.matrixWorldNeedsUpdate = true;
-    // this.helper.update();
-    // this.helper.position.copy(this.orthographicCamera.position);
-    // this.helper.rotation.copy(this.orthographicCamera.rotation);
   }
 }
